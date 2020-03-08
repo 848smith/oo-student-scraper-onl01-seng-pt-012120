1b80d2774e741d8dfac6ca043e2094acbf7559a2
Andrew Smith <848smith@gmail.com>
2020-03-08 23:17:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      student_details[:profile_url] = student.css("./fixtures/student-site/").text
+      student_details[:profile_url] = student.css("a.students/").text
       students << student_details
     end
     students
