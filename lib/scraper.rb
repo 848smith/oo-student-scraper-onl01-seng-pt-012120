11b73f4d4df4f0d529da59dee5a14331a721a027
Andrew Smith <848smith@gmail.com>
2020-03-08 23:04:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,8 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = File.read('https://learn-co-curriculum.github.io/student-scraper-test-page/')
+    scraper = Nokogiri::HTML(html)
+    
   end
 
   def self.scrape_profile_page(profile_url)
