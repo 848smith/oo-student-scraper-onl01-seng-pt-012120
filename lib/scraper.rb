0a803d974ba0b91ffa3dd7eb372dc753b478800b
Andrew Smith <848smith@gmail.com>
2020-03-08 23:06:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = File.read(
+    html = File.read(index_url)
     scraper = Nokogiri::HTML(html)
     hash = {}
     
